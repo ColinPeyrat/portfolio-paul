a10f6495600790414eeb161ed3582851a9ff75c9
vagrant <vagrant@local.dev>
2015-12-11 10:09:03 +0000

    add foundation and homepage

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 <html>
 	<head>
 		<meta charset=UTF-8 />
-		<link rel="stylesheet" href="style.css">
         <title>PHP</title>
-	</head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/foundation.min.css">
+        <link rel="stylesheet" href="css/foundation.css">
+        <link href="http://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css" rel="stylesheet">
+    </head>
 <?php
 require_once('connexion.php');
 
@@ -12,10 +16,130 @@ require_once('connexion.php');
 mysqli_close($CONNEXION);
 ?>
     <body>
-        <h2>Affichage de texte avec PHP</h2>
-        <p>
-            Cette ligne a été écrite entièrement en HTML.<br/>
-            <?php echo "Celle-ci a été écrite entièrement en PHP."; ?>
-        </p>
+    <div class="top-bar">
+        <div class="top-bar-left">
+            <ul class="dropdown menu" data-dropdown-menu>
+                <li class="menu-text">Paul Thibault</li>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Portfolio</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="callout large" id="bg">
+        <div class="row column text-center" id="me">
+            <h1>Paul Thibault</h1>
+            <p class="lead">Jacky tunning d'adobe premiere et d'after effect</p>
+<!--            <a href="#" class="button large">Learn More</a>-->
+<!--            <a href="#" class="button large hollow">Learn Less</a>-->
+        </div>
+    </div>
+    <div class="row wrapper">
+        <div class="medium-6 columns medium-push-6">
+            <img class="thumbnail" src="img/beau-male.jpg">
+        </div>
+        <div class="medium-6 columns medium-pull-6">
+            <h2>À propos de moi</h2>
+            <p>Bonjour, je m'appelle Paul.
+                Étudiant en MMI (Métiers du Multimédia et Internet), je suis passionné de vidéo.
+                Ce blog vous permettra de mieux me connaitre via mes réalisations, n'hesitez pas à me contacter !</p>
+        </div>
+    </div>
+    <div class="row wrapper">
+        <div class="medium-4 columns">
+            <h3>Photoshop</h3>
+            <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+        </div>
+        <div class="medium-4 columns">
+            <h3>Adobe premiere</h3>
+            <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+        </div>
+        <div class="medium-4 columns">
+            <h3>Touchage de nouille</h3>
+            <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+        </div>
+    </div>
+    <!--    <div class="row column">-->
+<!--        <h3>Our Recent Work</h3>-->
+<!--    </div>-->
+<!--    <div class="row medium-up-3 large-up-4">-->
+<!--        <div class="column">-->
+<!--            <img class="thumbnail" src="http://placehold.it/550x550">-->
+<!--        </div>-->
+<!--        <div class="column">-->
+<!--            <img class="thumbnail" src="http://placehold.it/550x550">-->
+<!--        </div>-->
+<!--        <div class="column">-->
+<!--            <img class="thumbnail" src="http://placehold.it/550x550">-->
+<!--        </div>-->
+<!--        <div class="column">-->
+<!--            <img class="thumbnail" src="http://placehold.it/550x550">-->
+<!--        </div>-->
+<!--        <div class="column">-->
+<!--            <img class="thumbnail" src="http://placehold.it/550x550">-->
+<!--        </div>-->
+<!--        <div class="column">-->
+<!--            <img class="thumbnail" src="http://placehold.it/550x550">-->
+<!--        </div>-->
+<!--        <div class="column">-->
+<!--            <img class="thumbnail" src="http://placehold.it/550x550">-->
+<!--        </div>-->
+<!--        <div class="column">-->
+<!--            <img class="thumbnail" src="http://placehold.it/550x550">-->
+<!--        </div>-->
+<!--        <div class="column">-->
+<!--            <img class="thumbnail" src="http://placehold.it/550x550">-->
+<!--        </div>-->
+<!--        <div class="column">-->
+<!--            <img class="thumbnail" src="http://placehold.it/550x550">-->
+<!--        </div>-->
+<!--        <div class="column">-->
+<!--            <img class="thumbnail" src="http://placehold.it/550x550">-->
+<!--        </div>-->
+<!--        <div class="column">-->
+<!--            <img class="thumbnail" src="http://placehold.it/550x550">-->
+<!--        </div>-->
+<!--    </div>-->
+<!--    <hr>-->
+
+    <footer class="footer">
+        <div class="row">
+            <div class="small-12 medium-6 large-5 columns">
+                <p class="logo"><i class="fi-shield"></i> STARWARS</p>
+                <p class="footer-links">
+                    <a href="#">Home</a>
+                    <a href="#">Blog</a>
+                    <a href="#">Pricing</a>
+                    <a href="#">About</a>
+                    <a href="#">Faq</a>
+                    <a href="#">Contact</a>
+                </p>
+                <p class="copywrite">Copywrite not copywrite © 2015</p>
+            </div>
+            <div class="small-12 medium-6 large-4 columns">
+                <ul class="contact">
+                    <li><p><i class="fi-marker"></i>1595 Spring Street New Britain, CT 06051</p></li>
+                    <li><p><i class="fi-telephone"></i>+1-656-453-9966</p></li>
+                    <li><p><i class="fi-mail"></i>contact@emperor.com</p></li>
+                </ul>
+            </div>
+            <div class="small-12 medium-12 large-3 columns">
+                <p class="about">About Star Wars</p>
+                <p class="about subheader">Strike me down, and I will become more powerful than you could possibly imagine.</p>
+                <ul class="inline-list social">
+                    <a href="#"><i class="fi-social-facebook"></i></a>
+                    <a href="#"><i class="fi-social-twitter"></i></a>
+                    <a href="#"><i class="fi-social-linkedin"></i></a>
+                    <a href="#"><i class="fi-social-github"></i></a>
+                </ul>
+            </div>
+        </div>
+    </footer>
+    <scritp type="text/javascript" src="js/vendor/jquery.min.js"></scritp>
+    <scritp type="text/javascript" src="js/vendor/what-input.min.js"></scritp>
+    <scritp type="text/javascript" src="js/foundation.min.js"></scritp>
+    <scritp type="text/javascript" src="js/app.js"></scritp>
+    </body>
     </body>
 </html>
