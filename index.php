d27192446a368de786b5f912ce27301a7969ffb2
vagrant <vagrant@local.dev>
2015-12-11 16:59:29 +0000

    correcting footer and add backgroud

--- index.php
+++ index.php
@@ -29,8 +29,8 @@ mysqli_close($CONNEXION);
 
     <div class="callout large" id="bg">
         <div class="row column text-center" id="me">
-            <h1>Paul Thibault</h1>
-            <p class="lead">Jacky tunning d'adobe premiere et d'after effect</p>
+            <h1 class="title">Paul Thibault</h1>
+            <p class="description">Jacky tunning d'adobe premiere et d'after effect</p>
 <!--            <a href="#" class="button large">Learn More</a>-->
 <!--            <a href="#" class="button large hollow">Learn Less</a>-->
         </div>
@@ -106,32 +106,25 @@ mysqli_close($CONNEXION);
     <footer class="footer">
         <div class="row">
             <div class="small-12 medium-6 large-5 columns">
-                <p class="logo"><i class="fi-shield"></i> STARWARS</p>
+                <p class="logo"><i class="fi-shield"></i> Paul Thibault</p>
                 <p class="footer-links">
-                    <a href="#">Home</a>
+                    <a href="#">Accueil</a>
                     <a href="#">Blog</a>
-                    <a href="#">Pricing</a>
-                    <a href="#">About</a>
-                    <a href="#">Faq</a>
-                    <a href="#">Contact</a>
+                    <a href="#">Portfolio</a>
                 </p>
-                <p class="copywrite">Copywrite not copywrite © 2015</p>
+                <p class="copywrite">All right reserved © 2015</p>
             </div>
             <div class="small-12 medium-6 large-4 columns">
                 <ul class="contact">
-                    <li><p><i class="fi-marker"></i>1595 Spring Street New Britain, CT 06051</p></li>
-                    <li><p><i class="fi-telephone"></i>+1-656-453-9966</p></li>
+                    <li><p><i class="fi-marker"></i>Annecy, France</p></li>
+                    <li><p><i class="fi-telephone"></i>06 j'ai oublié</p></li>
                     <li><p><i class="fi-mail"></i>contact@emperor.com</p></li>
                 </ul>
             </div>
             <div class="small-12 medium-12 large-3 columns">
-                <p class="about">About Star Wars</p>
-                <p class="about subheader">Strike me down, and I will become more powerful than you could possibly imagine.</p>
                 <ul class="inline-list social">
-                    <a href="#"><i class="fi-social-facebook"></i></a>
-                    <a href="#"><i class="fi-social-twitter"></i></a>
-                    <a href="#"><i class="fi-social-linkedin"></i></a>
-                    <a href="#"><i class="fi-social-github"></i></a>
+                    <a href="https://www.facebook.com/paulothibault"><i class="fi-social-facebook"></i></a>
+                    <a href="https://twitter.com/paul_thib"><i class="fi-social-twitter"></i></a>
                 </ul>
             </div>
         </div>
